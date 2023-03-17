c174a9cae88f4b3dd954f2406c890d45e21b855e
Matthew Clark <afromatt6288@hotmail.com>
2023-03-17 16:05:54 -0600

    Making good progress... might break it from here.

--- lib/animal.py
+++ lib/animal.py
@@ -1,2 +1,107 @@
+# from lib.animal import Animal
+# Sample Animals
+# Gertie = Animal("Gertie", "Giraffe", 200, "SanDeigoZoo")
+# Ellie = Animal("Ellie", "Elephant", 500, "DenZoo")
+# William = Animal("William", "Wolf", 80, "SanFranZoo")
+# George = Animal("George", "Gorilla", 320, "SanDiegoZoo")
+# Elijah = Animal("Elijah", "Elephant", 530, "DenZoo")
+# Artie = Animal("Artie", "Aardvark", 20, "SanFranZoo")
+
 class Animal:
-    pass
+
+    all = []
+    species_list = []
+
+    def __init__(self, nickname, species, weight, zoo):
+        if self.check_animal_nickname(nickname):
+            print("Animal already exists.")
+        else:
+            print(f"Welcome {nickname} to {zoo}! You are a(n) {species}, and weigh {weight} lbs. We are glad to have you!")
+            self.nickname = nickname                
+            self.species = species
+            self.weight = weight
+            self.zoo = zoo
+            Animal.add_to_animal_list(self)
+            Animal.update_species_list(species)  
+    
+    def get_animal_nickname(self):
+        return self._nickname
+    
+    def set_animal_nickname(self, nickname):
+        if self.check_animal_nickname(nickname):
+            print("Cannot change Animal Nickname!")
+        elif type(nickname) is str:
+            print(f"Setting Nickname to {nickname}")
+            self._nickname = nickname
+        else:
+            print("Animal Nickname must be a string")
+    
+    nickname = property(get_animal_nickname, set_animal_nickname)
+
+    def get_animal_species(self):
+        return self._species
+    
+    def set_animal_species(self, species):
+        if self.check_animal_species(species):
+            print("Cannot change Animal Species!")
+        elif type(species) is str:
+            print(f"Setting Species to {species}")
+            self._species = species
+        else:
+            print("Animal Species must be a string")
+    
+    species = property(get_animal_species, set_animal_species)
+
+    def get_animal_weight(self):
+        return self._weight
+    
+    def set_animal_weight(self, weight):
+        if type(weight) is int:
+            print(f"Setting Weight to {weight}")
+            self._weight = weight
+        else:
+            print("Animal Weight must be an Integer.")   
+    
+    weight = property(get_animal_weight, set_animal_weight)
+
+    def get_animal_zoo(self):
+        return self._zoo
+    
+    def set_animal_zoo(self, zoo):
+        if type(zoo) is str:
+            print(f"Setting Zoo to {zoo}")
+            self._zoo = zoo
+        else:
+            print("Animal Zoo must be a string")
+    
+    zoo = property(get_animal_zoo, set_animal_zoo)
+    
+    @classmethod
+    def check_animal_nickname(cls, nickname):
+        for animal in cls.all:
+            if nickname == animal.nickname:
+                return True 
+
+    @classmethod
+    def check_animal_species(cls, species):
+        for animal in cls.all:
+            if species == animal.species:
+                return True     
+    
+    @classmethod
+    def add_to_animal_list(cls, self):
+        cls.all.append(self)
+    
+    @classmethod
+    def update_species_list(cls, species):
+        if species in cls.species_list:
+            print(f"{species} already listed.")
+        else:
+            print(f"New Species ({species}) Found!")
+            cls.species_list.append(species) 
+    
+    @classmethod
+    def show_all_animals(cls):
+        for animal in cls.all:
+            print("Showing Animal")
+            print(f"{animal.nickname}: {animal.species}: {animal.weight}: {animal:zoo}")
--- lib/zoo.py
+++ lib/zoo.py
@@ -1,2 +1,59 @@
+# from lib.zoo import Zoo
+# Sample Zoos
+# SanFranZoo = Zoo("San Fransisco Zoo", "San Fransisco, CA")
+# SanDiegoZoo = Zoo("San Diego Zoo", "San Diego, CA")
+# DenZoo = Zoo("Denver Zoo", "Denver, CO")
+
 class Zoo:
+
+    all = []
+
+    def __init__(self, name, location):
+        if self.check_zoo(name):
+            print("Zoo already exists.")
+        else:
+            self.name = name
+            self.location = location
+            Zoo.add_to_zoo_list(self)
+    
+    def get_zoo_name(self):
+        return self._name
+    
+    def set_zoo_name(self, name):
+        if type(name) is str:
+            print(f"Setting name to {name}.")
+            self._name = name
+        else:
+            print("Zoo name must be a string.")    
+
+    name = property(get_zoo_name, set_zoo_name)
+
+    def get_zoo_location(self):
+        return self._location
+    
+    def set_zoo_location(self, location):
+        if type(location) is str:
+            print(f"Setting location to {location}.")
+            self._location = location
+        else:
+            print("Zoo location must be a string.")
+    
+    location = property(get_zoo_location, set_zoo_location)
+
+    @classmethod
+    def check_zoo(cls, name):
+        for zoo in cls.all:
+            if name == zoo.name:
+                return True            
+    
+    @classmethod
+    def add_to_zoo_list(cls, self):
+        cls.all.append(self)
+    
+    @classmethod
+    def show_all_zoos(cls):
+        for zoo in cls.all:
+            print("Showing Zoo")
+            print(f"{zoo.name}: {zoo.location}")
+    
     pass
